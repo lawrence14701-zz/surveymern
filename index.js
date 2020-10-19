ca36d0a231e3d76fd1e66052a83440ff41f2abfc
LAWRENCE NICASTRO <lawrence.nicastro1@gmail.com>
2020-10-18 20:53:00 -0400

    configure for heroku deployment

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const users = require('./routes/api/users');
-const tweets = require('./routes/api/tweets');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-require('./config/passport')(passport);
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+require("./config/passport")(passport);
 
 const server = express();
 //**! Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option*/
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 mongoose
-	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => console.log('Connected to MongoDB successfully'))
-	.catch((err) => console.log(err));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
 server.use(passport.initialize());
-server.use('/api/users', users);
-server.use('/api/tweets', tweets);
+server.use("/api/users", users);
+server.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
