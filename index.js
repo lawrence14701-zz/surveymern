b55c7af87f84735449e340c43d7e77b440777637
LAWRENCE NICASTRO <lawrence.nicastro1@gmail.com>
2020-10-18 21:13:55 -0400

    change port

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ server.use(passport.initialize());
 server.use("/api/users", users);
 server.use("/api/tweets", tweets);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-server.listen(port, () => console.log(`Server is running on port ${port}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
