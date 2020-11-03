dc2474db2569127ad5da7cf4d53bd31b6d3d2a8d
LAWRENCE NICASTRO <lawrence.nicastro1@gmail.com>
2020-11-03 04:49:59 -0500

    heroku

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-server.listen(port, () => console.log(`Server is running on port ${port}`));
+server.set("port", PORT);
+
+// server.listen(port, () => console.log(`Server is running on port ${port}`));
