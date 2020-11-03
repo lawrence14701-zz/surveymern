c83d3656a1b0103b7c088424cf6e131db7ca130e
LAWRENCE NICASTRO <lawrence.nicastro1@gmail.com>
2020-11-03 16:34:01 -0500

    change

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ server.use("/api/surveys", surveys);
 
 if (process.env.NODE_ENV === "production") {
   server.use(express.static("frontend/build"));
-  server.get("/", (req, res) => {
+  server.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
